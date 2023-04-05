de7ce59022029d55c30ce937ba92549a33501ba0
Sorin Marcu <ifoy.marcu@gmail.com>
2023-04-05 16:46:40 +0300

    Reset

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,4 @@
-const { defineConfig } = require("@vue/cli-service");
+const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  css: {
-    loaderOptions: {
-      sass: {
-        additionalData: `
-        @import "@/assets/styles/_functions.scss";
-        @import "@/assets/styles/_variables.scss";
-        @import "@/assets/styles/_mixins.scss";
-        `,
-      },
-    },
-  },
-});
+  transpileDependencies: true
+})
